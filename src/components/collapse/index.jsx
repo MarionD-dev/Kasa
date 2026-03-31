e90b5309b708e3f4a7d0988dd7e27e2a0730fd82
DIVERREZ <mdiverrez@mac-1.home>
2026-03-31 14:04:51 +0200

    create collapse and css

---
+++ src/components/collapse/index.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import './collapse.css'
+import arrow from '../../assets/arrow.png'
+
+function Collapse({ title, content }) {
+   const [isOpen, setIsOpen] = useState(false)
+   return (
+      <div className="collapse">
+         <div className="collapse__header" onClick={() => setIsOpen(!isOpen)}>
+            <span className={`collapse__arrow ${isOpen ? 'open' : ''}`}>
+            <img 
+                src={arrow} 
+                alt="arrow" 
+                className={`collapse__arrow ${isOpen ? 'open' : ''}`} 
+            />
+            </span>
+            <h3>{title}</h3>
+         </div>
+         {isOpen && (
+            <div className="collapse__content">
+               {content}
+            </div>
+         )}
+      </div>
+   )
+}
+
+export default Collapse
+    
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,13 @@
+import Collapse from "../../components/collapse"
+
 function About() {
-    return <div>Page About</div>
+    return (
+        <div>
+            <h1>About Us</h1>
+            <Collapse title="Our History" content="Learn about our company's journey and milestones." />
+            <Collapse title="Our Team" content="Meet the talented individuals behind our success." />
+        </div>
+    )
 }
 
 export default About
