e12f0338e2479451bd9b714bc8f0d7a0f9cb3bd5
DIVERREZ <mdiverrez@mac-1.home>
2026-03-31 17:00:35 +0200

    update footer code jsx and css

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,7 +5,6 @@ function Footer() {
    return (
       <footer className="footer">
          <img src={footerbanner} alt="Kasa banner" className="footer__banner" />
-         <p className="footer__text">© 2020 Kasa. All rights reserved</p>
       </footer>
    )
 }
