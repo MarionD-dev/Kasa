1765c2f9c47e384e2f5ce102851ca6be06a00fa3
DIVERREZ <mdiverrez@Host-001.lan>
2026-04-07 17:41:22 +0200

    fix logic logement page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import './error.css'   
 
 function Error() {
+    
     return (
         <div className="error">
             <h1>404</h1>
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 function Logement() {
-    return <div>Page Logement</div>
+    const { id } = useParams()
+    const [logement, setLogement] = useState(null)
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/api/properties/${id}`)
+            .then(response => response.json())
+            .then(data => setLogement(data))
+    }, [id])  
+
+    console.log(logement)
+    
+    
+    const handleNext = () => {
+        if (currentIndex === logement.pictures.length - 1) {
+            setCurrentIndex(0)
+        } else {
+            setCurrentIndex(currentIndex + 1)
+        }
+    }
+
+    const handlePrev = () => {
+    if (currentIndex === 0) {
+        setCurrentIndex(logement.pictures.length - 1)
+    } else {
+        setCurrentIndex(currentIndex - 1)
+    }
+}
+
+return <div>Page Logement {id}</div>
 }
 
 export default Logement
