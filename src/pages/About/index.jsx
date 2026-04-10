46c40952f635b38be5718e04c2f9d6d4fc4d6022
DIVERREZ <mdiverrez@mac-1.home>
2026-04-10 21:45:49 +0200

    feat: page about avec banner et collapses

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,19 @@
 import Collapse from "../../components/Collapse"
+import './about.css'
+import imgabout from '../../assets/imgabout.png'
 
 function About() {
     return (
         <div>
-            <h1>About Us</h1>
-            <Collapse title="Our History" content="Learn about our company's journey and milestones." />
-            <Collapse title="Our Team" content="Meet the talented individuals behind our success." />
+        <div className="banner">
+          <img src={imgabout} alt="Banner Image" />
+        </div>
+        <div className="about__infos">
+            <Collapse title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont vérifiées." />
+            <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme." />
+            <Collapse title="Service" content="La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance." />
+            <Collapse title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards soient bienrespectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+        </div>
         </div>
     )
 }
